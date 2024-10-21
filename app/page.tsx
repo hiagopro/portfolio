3551cbfe4266b9c38ab046185bb789173da3108b
hiago gentil <142757327+hiagopro@users.noreply.github.com>
2024-10-21 09:10:39 -0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FaHtml5, FaCss3Alt, FaReact, FaNodeJs } from "react-icons/fa"; // Font Awesome
-import { SiNextdotjs, SiTailwindcss } from "react-icons/si"; // Simple Icons
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 export default function Home() {
