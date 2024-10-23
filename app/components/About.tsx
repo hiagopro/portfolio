2d3ac17aff444390ff1131f12bda9676e3f74c59
hiago gentil <142757327+hiagopro@users.noreply.github.com>
2024-10-23 10:45:14 -0300

    ok

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-
-
+import Image from "next/image";
+import {  NotepadText } from "lucide-react";
 const About: React.FC = () => {
-  return(
-    <div>
-        
+  return (
+    <div className="bg-surface-terciari flex flex-col gap-8">
+      <div className="flex flex-col justify-center">
+        <Image className="" src="/mancomputer.svg" width="400" height="300" alt="" />
+
+        <div className="flex gap-6 pl-8 mt-0 ">
+          <i className="devicon-javascript-plain text-primary-color-400 text-4xl"></i>
+          <i className="devicon-react-original  text-primary-color-400 text-4xl"></i>
+          <i className="devicon-mysql-original text-primary-color-400 text-4xl"></i>
+          <i className="devicon-tailwindcss-original text-primary-color-400 text-4xl"></i>
+          <i className="devicon-nodejs-plain-wordmark text-primary-color-400 text-4xl"></i>
+        </div>
+      </div>
+      <div>
+        <h2 className="font-kalam text-2xl text-secondary-color pl-8">
+          About me
+        </h2>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h1 className="pl-8 text-3xl text-primary-text font-Heebo font-bold">
+            Eu sou um apaixonado pelo desenvolvimento web a procura de minha primeira experiencia profissional
+        </h1>
+        <p className="pl-8 text-xl text-secondary-text font-Heebo">alem de codar, sou um amante de esportes, gosto de treinar, jogar futebol e futvolei. gosto de sair com a familia e momentos ao ar livre.</p>
+      </div>
+      <div className="pl-8"> <button className="flex gap-1 py-4 px-6 text-nowrap font-Heebo text-primary-text rounded-lg bg-primary-color-500 hover:bg-primary-color-400 lg:px-6 lg:py-4">
+            <NotepadText /> Meu resumo{" "}
+          </button></div>
     </div>
   );
 };
-export default About
+export default About;
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ import { ChevronsRight, NotepadText } from "lucide-react";
 
 const Hero: React.FC = () => {
   return (
-    <div className="bg-surface-background md:flex md:flex-row md:px-10 md:justify-between lg:justify-center lg:gap-32 lg:py-24">
+    <div className=" py-5 relative bg-surface-background md:flex md:flex-row md:px-10 md:justify-between lg:justify-center lg:gap-32 lg:py-24">
       <div className=" flex  justify-center md:order-2 md:justify-center">
         <Image src="/computer.svg" width="300" height="300" alt="" />
       </div>
@@ -25,10 +25,28 @@ const Hero: React.FC = () => {
           </p>
         </div>
         <div className="px-12 flex gap-5 py-6 ">
-          <button className="flex gap-1 py-3 px-4 text-nowrap font-Heebo text-primary-text rounded-lg bg-surface-secondary hover:bg-surface-terciari lg:px-6 lg:py-4"><NotepadText /> Meu resumo </button>
-          <button className="flex py-3 px-4 text-nowrap font-Heebo text-primary-text rounded-lg bg-primary-color-500 hover:bg-primary-color-400 lg:py-4 lg:px-6" >Get in touch<ChevronsRight /></button>
+          <button className="flex gap-1 py-3 px-4 text-nowrap font-Heebo text-primary-text rounded-lg bg-surface-secondary hover:bg-surface-terciari lg:px-6 lg:py-4">
+            <NotepadText /> Meu resumo{" "}
+          </button>
+          <button className="flex py-3 px-4 text-nowrap font-Heebo text-primary-text rounded-lg bg-primary-color-500 hover:bg-primary-color-400 lg:py-4 lg:px-6">
+            Get in touch
+            <ChevronsRight />
+          </button>
         </div>
       </div>
+      <svg
+        className="w-20 h-20  text-secondary-color rotate-180 absolute bottom-[-45px] right-10 "
+        viewBox="0 0 69 90"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0.563538 86.4441C9.62047 89.2172 16.6734 91.0373 25.3582 87.062C32.131 83.9619 38.9967 80.1472 44.485 75.0561C50.4537 69.5194 57.9022 61.3399 53.1364 53.1378C49.3227 46.5744 43.2597 40.2549 43.2597 32.3089C43.2597 23.5235 48.7541 18.3068 54.9775 12.9941C58.6952 9.82034 63.0893 5.84965 65.693 1.65486C66.8291 -0.175672 57.9801 2.37935 56.7613 2.69535C56.4809 2.76805 57.7534 3.40302 58.0609 3.80379C59.7892 6.05612 60.7498 8.83608 62.4864 11.1169C66.0039 15.7368 66.9283 3.54324 68.4493 1.40005"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+      </svg>
     </div>
   );
 };
